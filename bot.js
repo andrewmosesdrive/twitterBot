097037a5bbb4efba2a11e057f56b0802de05d028
Andrew Moses <andrewmosesdrive@gmail.com>
2021-04-06 22:05:43 -0700

    fixed config vars

--- bot.js
+++ bot.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const twit = require('twit');
 const _ = require('lodash');
+const config = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+}
 
 const T = new twit(config);
 
