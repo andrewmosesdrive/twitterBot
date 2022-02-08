6b42c3c097d726b8cec4a3c7dbb3b3ecaf3567aa
Andrew Moses <andrewmosesdrive@gmail.com>
2022-02-07 19:03:16 -0700

    upgraded twitterbot to zimmerbot

--- bot.js
+++ bot.js
@@ -1,46 +1,78 @@
-require('dotenv').config();
-const twit = require('twit');
-const _ = require('lodash');
+require("dotenv").config();
+const twit = require("twit");
+const _ = require("lodash");
 const config = {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-}
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+};
 
 const T = new twit(config);
 
-let siegwardQuotes = [
-    'Hmm... Mmmmmm...',
-    'Pardon me, I was absorbed in thought.',
-    "To be honest, I'm in a bit of a pickle.",
-    "Have you ever walked near a white birch, only to be struck by a great arrow?",
-    "I've got to use my head. And think.",
-    "Don't disappear like that. You had me downright worried.",
-    "Thanks to you, an epiphany has struck me square in the head.",
-    "I've unravelled the riddle of this inscrutable lift!",
-    "I'm not exactly sure what happened.",
-    "Now, now, think twice before you go down that road.",
-    "Well, it's too late now.",
-    "We Unkindled must put our duties first.",
-    "To your valour, my sword, and our victory together. Long may the Sun shine!",
-    "Well, I'm going to have myself a little nap. The only thing to do, really, after a nice toast.",
-    "I'm loathe to admit it, but I've been had.",
-    "I'm in quite a pickle indeed.",
-    "On some days, I begin to doubt myself.",
-    "I'm no coward, and I've a steady hand, but that thing makes my skin crawl.",
-    "Long may the Sun shine!",
-    "Great planning begets great fortune. An old tenet of the Knights of Catarina.",
-    "My liberation requires a key. But the key is outside... Hmm... A riddle for the ages..."
+let responses = [
+  "git gud scrub",
+  "get off twitter nerd",
+  "how's your blood pressure?",
+  "WWMZD?",
+  "you should delete this app, it's bad for you",
+  "trust the process bro",
+  "#sprots",
+  "OK boomer",
+  "typical fedboi",
 ];
 
-let quote = _.sample(siegwardQuotes);
-
-function postTweet() {
-    console.log('Siegward just tweeted: ' + quote)
-    T.post('statuses/update', { status: quote })
+function replyToTweet(idString, reply) {
+  T.post(
+    "statuses/update",
+    {
+      status: reply,
+      in_reply_to_status_id: idString,
+    },
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 }
 
-postTweet();
+function getTweets() {
+  T.get(
+    "statuses/user_timeline",
+    { user_id: "49189455" },
+    (error, data, res) => {
+      const mostRecent = data.shift();
+      const id = mostRecent.id_str;
+      const reply = "@_jason_777" + " " + _.sample(responses);
+      replyToTweet(id, reply);
+      console.log("reply sent");
+    }
+  );
+}
 
+getTweets();
 
+// create a tweet, storing the id_str of the tweet we commented on for later reference
+// function tweetRecord(postId) {
+//   T.post(
+//     "statuses/update",
+//     { status: "#gotem " + "#luckyNumber" + postId + " #botLife" },
+//     (err) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//     }
+//   );
+// }
+// check own post to see if we already responded
+// function getRecord() {
+//   T.get("statuses/user_timeline", {
+//     user_id: "1378901520184344576",
+//   })
+//     .catch((err) => console.log(err))
+//     .then((result) => {
+//       // get lucky number from most recent tweet
+//       console.log(result.data[1].text);
+//     });
+// }
